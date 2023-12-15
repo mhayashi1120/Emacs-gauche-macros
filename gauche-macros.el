c9e76fbd1632e556cca40a9da4d98330bf29285a
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-12-15 21:34:51 +0900

    revision up

--- gauche-macros.el
+++ gauche-macros.el
@@ -5,7 +5,7 @@
 ;; Emacs: GNU Emacs
 ;; URL: https://github.com/mhayashi1120/Emacs-gauche-macros
 ;; Package-Requires: ((emacs "24.3"))
-;; Version: 0.8.1
+;; Version: 0.8.2
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
